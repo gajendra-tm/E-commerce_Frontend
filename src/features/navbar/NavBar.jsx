ced3df7e5a0b330f9303b446353f86249ed77797
Gajendra <gajendratm05@gmail.com>
2023-12-27 15:17:58 +0530

    added my-profile, my-orders page navigations

--- src/features/navbar/NavBar.jsx
+++ src/features/navbar/NavBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { useAnimate, stagger } from "framer-motion";
+import { useAnimate, stagger, motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { seletCartItems } from "../cart/cartSlice";
 
 const navLists = [
   { name: "Dashboard", href: "#" },
@@ -9,7 +11,15 @@ const navLists = [
   { name: "Calendar", href: "#" },
 ];
 
-// for animation
+const userMenu = [
+  { name: "My Profile", href: "/my-profile" },
+  { name: "My Orders", href: "/my-orders" },
+  { name: "Sign out", href: "/" },
+];
+
+const staggerWebItems = stagger(0.01, { startDelay: 0.1 });
+
+// for the animation
 const Path = (props) => (
   <path
     fill="transparent"
@@ -65,10 +75,46 @@ function useMenuAnimation(isOpen) {
   return scope;
 }
 
+function useUserAnimation(userIsOpen) {
+  const [userScope, animate] = useAnimate();
+
+  useEffect(() => {
+
+    animate(
+      "ul",
+      {
+        clipPath: userIsOpen
+          ? "inset(0% 0% 0% 0% round 10px)"
+          : "inset(10% 50% 90% 50% round 10px)",
+      },
+      {
+        type: "spring",
+        bounce: 0,
+        duration: 0.5,
+      }
+    );
+
+    animate(
+      "li",
+      userIsOpen
+        ? { opacity: 1, scale: 1, filter: "blur(0px)" }
+        : { opacity: 0, scale: 0.3, filter: "blur(20px)" },
+      {
+        duration: 0.2,
+        delay: userIsOpen ? staggerWebItems : 0,
+      }
+    );
+  }, [userIsOpen]);
+
+  return userScope;
+}
+
 export default function NavBar({ children }) {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [userIsOpen, setUserIsOpen] = useState(false);
+  const userScope = useUserAnimation(userIsOpen);
   const scope = useMenuAnimation(isOpen);
+  const cartItems = useSelector(seletCartItems);
 
   return (
     <>
@@ -76,11 +122,11 @@ export default function NavBar({ children }) {
         <nav className="flex max-w-full z-20 md:z-10 h-12 sm:h-16 p-1 box-content justify-between items-center bg-gray-800 fixed top-0 left-0 right-0">
           <div className="h-12 w-12 sm:h-14 sm:w-14 ml-10 rounded-full bg-white">
             <Link to="/">
-            <img
-              className="h-full w-full object-cover object-center"
-              src="/images/cart-logo.png"
-              alt="logo"
-            />
+              <img
+                className="h-full w-full object-cover object-center"
+                src="/images/cart-logo.png"
+                alt="logo"
+              />
             </Link>
           </div>
 
@@ -99,24 +145,48 @@ export default function NavBar({ children }) {
               })}
             </ul>
           </div>
-          <div className=" flex justify-between items-center relative w-20 h-14 ml-auto sm:ml-0 mr-5 sm:mr-10">
-           <Link to="/cart">
-            <span>
-              <svg
-                className="invert"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path d="M4.00488 16V4H2.00488V2H5.00488C5.55717 2 6.00488 2.44772 6.00488 3V15H18.4433L20.4433 7H8.00488V5H21.7241C22.2764 5 22.7241 5.44772 22.7241 6C22.7241 6.08176 22.7141 6.16322 22.6942 6.24254L20.1942 16.2425C20.083 16.6877 19.683 17 19.2241 17H5.00488C4.4526 17 4.00488 16.5523 4.00488 16ZM6.00488 23C4.90031 23 4.00488 22.1046 4.00488 21C4.00488 19.8954 4.90031 19 6.00488 19C7.10945 19 8.00488 19.8954 8.00488 21C8.00488 22.1046 7.10945 23 6.00488 23ZM18.0049 23C16.9003 23 16.0049 22.1046 16.0049 21C16.0049 19.8954 16.9003 19 18.0049 19C19.1095 19 20.0049 19.8954 20.0049 21C20.0049 22.1046 19.1095 23 18.0049 23Z"></path>
-              </svg>
-              <span className="text-center absolute rounded-full top-1.5 right-10 w-5 h-5 z-10 bg-red-500 text-sm font-medium text-gray-100 ">
-                3
+          <div className=" flex justify-between items-center relative w-20 h-14 ml-auto sm:ml-0 mr-5 sm:mr-10" ref={userScope}>
+            <Link to="/cart">
+              <span>
+                <svg
+                  className="invert"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path d="M4.00488 16V4H2.00488V2H5.00488C5.55717 2 6.00488 2.44772 6.00488 3V15H18.4433L20.4433 7H8.00488V5H21.7241C22.2764 5 22.7241 5.44772 22.7241 6C22.7241 6.08176 22.7141 6.16322 22.6942 6.24254L20.1942 16.2425C20.083 16.6877 19.683 17 19.2241 17H5.00488C4.4526 17 4.00488 16.5523 4.00488 16ZM6.00488 23C4.90031 23 4.00488 22.1046 4.00488 21C4.00488 19.8954 4.90031 19 6.00488 19C7.10945 19 8.00488 19.8954 8.00488 21C8.00488 22.1046 7.10945 23 6.00488 23ZM18.0049 23C16.9003 23 16.0049 22.1046 16.0049 21C16.0049 19.8954 16.9003 19 18.0049 19C19.1095 19 20.0049 19.8954 20.0049 21C20.0049 22.1046 19.1095 23 18.0049 23Z"></path>
+                </svg>
+                {cartItems && (
+                  <span className="text-center absolute box-content rounded-full top-1.5 right-10 w-5 h-5 z-10 bg-red-500 text-sm font-medium text-gray-100 ">
+                    {cartItems.length}
+                  </span>
+                )}
               </span>
-            </span>
             </Link>
-            <div className="text-gray-300 ml-4 text-base font-medium">user</div>
+            <motion.button
+              whileTap={{ scale: 0.97 }}
+              onClick={() => setUserIsOpen(!userIsOpen)}
+              className="text-gray-300 ml-8 text-base font-medium"
+            >
+              User
+            </motion.button>
+              <ul
+              style={{
+              pointerEvents: userIsOpen ? "auto" : "none",
+              clipPath: "inset(10% 50% 90% 50% round 10px)",
+            }}
+            className=" bg-gray-800 text-gray-300 top-14 md:top-16 left-3 w-28 h-fit p-2 absolute"
+            onClick={() => setUserIsOpen(!userIsOpen)}
+              >
+                {userMenu.map((menu,menuIdx) => {
+                  return (
+                    <li key={menuIdx} className="flex flex-col items-center rounded-md w-full hover:bg-gray-500">
+                      <Link to={menu.href}>{menu.name}</Link>
+                    </li>
+                  );
+                })}
+              </ul>
           </div>
 
           {/* mobile layout */}
@@ -155,14 +225,11 @@ export default function NavBar({ children }) {
             </div>
           </div>
         </nav>
-
         <header className="  px-2 shadow-xl mt-14 border-b-2 border-gray-400 sm:mt-20 h-9 sm:h-11 flex">
           <h1 className=" flex items-start text-2xl font-bold">E-commerce</h1>
         </header>
-        <main className=" bg-gray-100 min-h-full">
-          <div className=" bg-white min-h-full rounded-lg">
-            {children}
-          </div>
+        <main className=" bg-gray-300 min-h-full left-0 p-1 md:p-5">
+          <div className=" bg-white p-5 min-h-full rounded-lg">{children}</div>
         </main>
       </div>
     </>
