ceaf9ba3f072c73b9d9a3288c60a27d2bb00b2ff
Gajendra <gajendratm05@gmail.com>
2024-04-17 09:57:08 +0530

    made changes to logout functionality

--- src/features/auth/component/SignOut.jsx
+++ src/features/auth/component/SignOut.jsx
@@ -6,7 +6,6 @@ import { Navigate } from "react-router-dom";
 export default function SignOut() {
   const dispatch = useDispatch();
   const user = useSelector(selectLoggedInUser);
-
   useEffect(() => {
     dispatch(signOutAsync());
   });
