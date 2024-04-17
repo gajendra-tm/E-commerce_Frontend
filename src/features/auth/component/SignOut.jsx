ec1ac545179c94ec6b8b915f6278884c5c86e349
Gajendra <gajendratm05@gmail.com>
2024-04-17 11:10:06 +0530

    modified logout info

--- src/features/auth/component/SignOut.jsx
+++ src/features/auth/component/SignOut.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectLoggedInUser, signOutAsync } from "../authSlice";
 import { Navigate } from "react-router-dom";
+import { fetchLoggedInUserInfoAsync } from "../../user/userSlice";
 
 export default function SignOut() {
   const dispatch = useDispatch();
   const user = useSelector(selectLoggedInUser);
   useEffect(() => {
     dispatch(signOutAsync());
+    dispatch(fetchLoggedInUserInfoAsync());
   });
 
   return <>{!user && <Navigate to="/login" replace={true}></Navigate>}</>;
